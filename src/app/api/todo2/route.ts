d592a41e7a4f8de109fc2bca45a65977e01a84f8
jbtaku <jbta1086@gmail.com>
2024-01-07 21:14:21 +0900

    2024-01-07-35

--- src/app/api/todo2/route.ts
+++ src/app/api/todo2/route.ts
@@ -1,8 +1,6 @@
 import prisma from "@/lib/prisma/prisma";
 import { NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic"
-
 export const GET = async () => {
   const todoList = await prisma.todo.findMany();
   return NextResponse.json(todoList);
