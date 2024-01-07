d5573d8cff007db1e71c177cbd49c74f5731776a
jbtaku <jbta1086@gmail.com>
2024-01-07 18:42:10 +0900

    2024-01-07-27

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import SignOutButton from "@/components/custom-ui/SignOutButton";
 import CreateTodo from "./components/CreateTodo";
 import TodoList from "./components/TodoList";
+import { fetcher } from "@/utils/fetcher";
+import { Todo } from "@prisma/client";
 
 async function page() {
+  const a = await fetcher<Todo[]>(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/todo`,
+    {
+      next: { tags: ["todo"] },
+    }
+  );
   return (
     <div className="space-y-8">
+      {a?.map((item)=>{
+        return <p>{item.content}</p>
+      })}
       <SignOutButton />
       <div className="border-4 border-green-600 p-6 space-y-4">
         <h2 className="text-4xl font-bold">todo Area</h2>
